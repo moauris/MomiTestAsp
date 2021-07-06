a5107a2022fdf564d77288943f637dd3f78c6988
unknown <mchenf@icloud.com>
2021-07-07 02:28:53 +0800

    Built a better mysql schema, injected some sample data

--- BXSim_MySQL_Migrations/INIT_DB_TABLES.sql
+++ BXSim_MySQL_Migrations/INIT_DB_TABLES.sql
@@ -1,56 +1,98 @@
-CREATE SCHEMA IF NOT EXISTS BXSimDB CHARACTER SET utf8;
-USE BXSimDB;
-CREATE TABLE IF NOT EXISTS PracticeSets (
-    id INT UNSIGNED NOT NULL,
+CREATE SCHEMA IF NOT EXISTS db_bxsim CHARACTER SET utf8;
+
+USE db_bxsim;
+
+CREATE TABLE IF NOT EXISTS tbl_practicesets (
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     PracticeName VARCHAR(100) NOT NULL UNIQUE,
-    PracticeIntroPage TEXT,
-    PRIMARY KEY (id)
-) ENGINE=INNODB;
-CREATE TABLE IF NOT EXISTS Scenarios (
-    id INT UNSIGNED NOT NULL,
-    ScenarioPage TEXT,
-    PracticeID INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (PracticeID)
-        REFERENCES PracticeSets(id)
-        ON DELETE CASCADE
-) ENGINE=INNODB;
-CREATE TABLE IF NOT EXISTS Quizzes (
-    id INT UNSIGNED NOT NULL,
-    Question VARCHAR(255),
-    ScenarioID INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (ScenarioID)
-        REFERENCES Scenarios(id)
-        ON DELETE CASCADE
-) ENGINE=INNODB;
-CREATE TABLE IF NOT EXISTS rel_OptionQuiz (
-    id INT UNSIGNED NOT NULL,
-    QuizzesID INT REQUIRED,
-    OptionID INT REQUIRED,
-    AnswerState TINYINT REQUIRED,
-    PRIMARY KEY (id),
-    FOREIGN KEY (QuizzesID)
-        REFERENCES Quizzes(id)
-        ON DELETE CASCADE,
-    FOREIGN KEY (OptionID)
-        REFERENCES Options(id)
-        ON DELETE CASCADE
-) ENGINE=INNODB;
-CREATE TABLE IF NOT EXISTS Options (
-    id INT UNSIGNED NOT NULL,
-    Content VARCHAR(255) REQUIRED,
-    PRIMARY KEY (id)
+    PracticeIntroPage TEXT
 ) ENGINE=INNODB;
 
-INSERT INTO PracticeSets (PracticeName, PracticeIntroPage) VALUES (
+CREATE TABLE IF NOT EXISTS tbl_scenarios (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    ScenarioPage TEXT,
+    PracticeID INT NOT NULL REFERENCES tbl_practicesets(id) 
+        ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS tbl_quizzes (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Question VARCHAR(255),
+    ScenarioID INT NOT NULL REFERENCES tbl_scenarios(id)
+        ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS tbl_options (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Content VARCHAR(255) NOT NULL
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS rel_optionquiz (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    QuizzesID INT NOT NULL REFERENCES tbl_quizzes(id)
+        ON DELETE CASCADE,
+    OptionID INT NOT NULL REFERENCES tbl_options(id)
+        ON DELETE CASCADE,
+    AnswerState TINYINT NOT NULL
+) ENGINE=INNODB;
+
+INSERT INTO tbl_practicesets (PracticeName, PracticeIntroPage) VALUES (
     'Technical Rehydration - C# CLR',
     '<p>Technical Rehydration series are designed to quickly pick-up knowledge points from past learning sessions.</p><p>For this particular practice set we focus on the CLR mechanisms in C#. Most knowledge points can be found in the book <i>CLR via C#</i>.</p>'
 );
 
-SET @max_id = (SELECT MAX(id) FROM PracticeSets);
-INSERT INTO Scenarios (ScenarioPage, PracticeID) VALUES (
-    '<p>In C#, we often use the new operator</p>',
-    @max_id
+INSERT INTO tbl_scenarios (ScenarioPage, PracticeID) VALUES (
+    'In C#, we often use the new operator to initialize new instance of a type.',
+    @prac_id:=(SELECT MAX(id) FROM tbl_practicesets)
 );
-\G
+
+INSERT INTO tbl_options (Content) VALUES ('Yes');
+INSERT INTO tbl_options (Content) VALUES ('No');
+INSERT INTO tbl_quizzes (Question, ScenarioID) VALUES (
+    "What is the first thing the 'new' operator does?",
+    @scen_id:=(SELECT MAX(id) FROM tbl_scenarios)
+);
+
+SET @quiz_id:=(SELECT MAX(id) FROM tbl_quizzes);
+
+INSERT INTO tbl_options (Content) VALUES (
+    'Request to allocate memory on heap'
+);
+
+SET @opts_id:=(SELECT MAX(id) FROM tbl_options);
+INSERT INTO rel_OptionQuiz (QuizzesID, OptionID, AnswerState) VALUES (
+    @quiz_id, @opts_id, 0
+);
+
+INSERT INTO tbl_options (Content) VALUES (
+    "Call the type's constructor function to initialize the instance"
+);
+
+SET @opts_id:=(SELECT MAX(id) FROM tbl_options);
+INSERT INTO rel_OptionQuiz (QuizzesID, OptionID, AnswerState) VALUES (
+    @quiz_id, @opts_id, -1
+);
+
+INSERT INTO tbl_options (Content) VALUES (
+    "Build resources that this instance is trying to access"
+);
+
+SET @opts_id:=(SELECT MAX(id) FROM tbl_options);
+INSERT INTO rel_OptionQuiz (QuizzesID, OptionID, AnswerState) VALUES (
+    @quiz_id, @opts_id, -1
+);
+
+INSERT INTO tbl_options (Content) VALUES (
+    "Validates the security requirements to prevent memory leakage"
+);
+
+SET @opts_id:=(SELECT MAX(id) FROM tbl_options);
+INSERT INTO rel_OptionQuiz (QuizzesID, OptionID, AnswerState) VALUES (
+    @quiz_id, @opts_id, -1
+);
+
+SELECT * FROM tbl_quizzes q 
+    INNER JOIN rel_optionquiz l ON (
+        q.id = l.QuizzesID) 
+    INNER JOIN tbl_options o ON (
+        o.id = l.OptionID) \G
